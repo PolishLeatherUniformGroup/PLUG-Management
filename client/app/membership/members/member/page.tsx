820cc0bd3f10265622a78f0a898db35e4dba9530
Tomasz Molis <molitom@chrobinson.com>
2024-02-07 14:50:13 +0100

    memebrs

---
+++ client/app/membership/members/member/page.tsx
@@ -0,0 +1,5 @@
+export default function Member(){
+    return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Członek</h1>
+    </main>
+}
---
+++ client/app/membership/members/page.tsx
@@ -0,0 +1,5 @@
+export default function Members(){
+    return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Członkowie</h1>
+    </main>
+}
