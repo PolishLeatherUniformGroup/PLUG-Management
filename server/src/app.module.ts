426298fc7b578e62135d52f9caa44702051b9428
Tomasz Molis <tomasz.molis@yahoo.com>
2024-02-07 08:53:58 +0100

    remove unused components

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApplyModule } from './apply/apply.module';
 
 @Module({
   imports: [ApplyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
