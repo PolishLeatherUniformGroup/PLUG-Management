27cc97a771f3b6cd068a6f84b19bed5f12995947
Tomasz Molis <tomasz.molis@yahoo.com>
2024-02-09 15:39:23 +0100

    fix applicants

--- client/app/admin/applicants/page.tsx
+++ client/app/admin/applicants/page.tsx
@@ -8,32 +8,33 @@ import { Applicant } from "@/app/models/apply/application.model";
 
 
 export default function AdminApplicants() {
-    return <div className="grid grid-cols-12 gap-2">
-
-        <div className="bg-content1 p-4 drop-shadow-sm rounded-lg col-span-7">
-            <FontAwesomeIcon icon={faFilter} className="text-default-600 fa-1x" />
-            <span className="text-default-600 font-bold">Filtry</span>
-            <ButtonGroup className="mx-2">
-                <Button><FontAwesomeIcon icon={faLayerGroup} />Wszystkie</Button>
-                <Button><FontAwesomeIcon icon={faPaperPlane} />Nowe</Button>
-                <Button><FontAwesomeIcon icon={faHourglass} />W trakcie</Button>
-                <Button><FontAwesomeIcon icon={faCheck} />Zakceptowane</Button>
-                <Button><FontAwesomeIcon icon={faXmark} />Odrzucone</Button>
-            </ButtonGroup>
+    return <div className="grid grid-cols-12 gap-2 auto-rows-min">
+        <div className="col-span-7">
+            <div className="bg-content1 p-4 drop-shadow-sm rounded-lg mb-2">
+                <FontAwesomeIcon icon={faFilter} className="text-default-600 fa-1x" />
+                <span className="text-default-600 font-bold">Filtry</span>
+                <ButtonGroup className="mx-2">
+                    <Button><FontAwesomeIcon icon={faLayerGroup} />Wszystkie</Button>
+                    <Button><FontAwesomeIcon icon={faPaperPlane} />Nowe</Button>
+                    <Button><FontAwesomeIcon icon={faHourglass} />W trakcie</Button>
+                    <Button><FontAwesomeIcon icon={faCheck} />Zakceptowane</Button>
+                    <Button><FontAwesomeIcon icon={faXmark} />Odrzucone</Button>
+                </ButtonGroup>
+            </div><Table radius="sm">
+                <TableHeader>
+                    <TableColumn>Imię</TableColumn>
+                    <TableColumn>Nazwisko</TableColumn>
+                    <TableColumn>Email</TableColumn>
+                    <TableColumn>Data wniosku</TableColumn>
+                    <TableColumn>Status</TableColumn>
+                </TableHeader>
+                <TableBody emptyContent={"Brak aplikacji"}>{[]}</TableBody>
+            </Table>
         </div>
         <div className="col-span-5 row-span-2 ">
-            <ApplicationPreview applicant={{}as Applicant} />
+            <ApplicationPreview applicant={{} as Applicant} />
         </div>
-        <Table radius="sm" className="col-span-7">
-            <TableHeader>
-                <TableColumn>Imię</TableColumn>
-                <TableColumn>Nazwisko</TableColumn>
-                <TableColumn>Email</TableColumn>
-                <TableColumn>Data wniosku</TableColumn>
-                <TableColumn>Status</TableColumn>
-            </TableHeader>
-            <TableBody emptyContent={"Brak aplikacji"}>{[]}</TableBody>
-        </Table>
+
 
 
     </div>
---
+++ client/app/membership/members/member/page.tsx
@@ -0,0 +1,5 @@
+export default function Member(){
+    return (<main className="">
+        <h1>Członek</h1>
+    </main>)
+}
