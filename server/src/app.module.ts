7966b5fd17486b9e510c7cac72915fe7ca9f14e0
Tomasz Molis <tomasz.molis@yahoo.com>
2024-02-11 23:45:20 +0100

    Add extra modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ApplyModule } from './apply/apply.module';
 import { MembershipModule } from './membership/membership.module';
+import { EventsModule } from './events/events.module';
+import { VotingModule } from './voting/voting.module';
+import { TasksModule } from './tasks/tasks.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
-  imports: [ApplyModule, MembershipModule],
+  imports: [ApplyModule, MembershipModule, EventsModule, VotingModule, TasksModule, SettingsModule],
   controllers: [],
   providers: [],
 })
