1f68da2871ebbbd93ab8506eda4a88f71932b272
Tomasz Molis <tomasz.molis@yahoo.com>
2024-02-14 09:57:48 +0100

    Update apply.controller.ts

--- server/src/apply/infrastructure/controller/apply.controller.ts
+++ server/src/apply/infrastructure/controller/apply.controller.ts
@@ -207,22 +207,4 @@ export class ApplyController {
         }
     }
 
-    @Get('applicants/:id')
-    @ApiOperation({ summary: 'Get Applicant by Id.' })
-    @ApiResponse({ status: 200, description: 'Applicant', type: ApplicantDto})
-    @ApiParam({ name: 'id', required: true, description: 'Applicant Id', type: 'string'})
-    async getApplication(@Param() id:string):Promise<ApplicantDto | null> {
-        const query = new GetApplicantQuery(id);
-        console.log('Async GetApplicantQuery...',query);
-        const applicant = await this.queryBus.execute(query);
-        return applicant;
-    }
-
-    @Get('applicants/:id/recommendations')
-    @ApiOperation({ summary: 'Get Applicant Recommendations  by  applicants Id.' })
-    @ApiResponse({ status: 200, description: 'Recommendations', type: RecommendationView})
-    @ApiParam({ name: 'id', required: true, description: 'Applicant Id', type: 'string'})
-    async getApplicationRecommendations(@Param() id:string):Promise<RecommendationView[]> {
-        return [];
-    }
 }
