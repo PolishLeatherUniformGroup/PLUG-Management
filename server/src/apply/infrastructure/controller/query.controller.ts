d105b73ff81708ddd0f7b9b4b1c5522463e62e78
Tomasz Molis <tomasz.molis@yahoo.com>
2024-02-14 09:56:44 +0100

    Update query.controller.ts

--- server/src/apply/infrastructure/controller/query.controller.ts
+++ server/src/apply/infrastructure/controller/query.controller.ts
@@ -1,4 +1,12 @@
+import { Body, Controller, Get, Param } from "@nestjs/common";
+import { ApplyRequestDto } from "../dto/apply-request.dto";
+import { ApiOperation, ApiResponse, ApiTags, ApiParam } from "@nestjs/swagger";
+import {QueryBus } from "@nestjs/cqrs";
+
 @Controller('query')
 @ApiTags('apply')
 export class QueryController{
+    constructor(
+        private readonly queryBus: QueryBus
+    ){}
 }
