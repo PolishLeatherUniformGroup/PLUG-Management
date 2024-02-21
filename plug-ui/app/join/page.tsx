075a8905f053b79619ea8b301a2d7e4591f9c31f
Tomasz Molis <molitom@chrobinson.com>
2024-02-21 12:43:46 +0100

    hero page

---
+++ plug-ui/app/join/page.tsx
@@ -0,0 +1,8 @@
+import { ApplyForm } from "../components/apply-form";
+
+export default function Join(){
+    return (<div >
+        <ApplyForm />
+      </div>
+    );
+}
--- plug-ui/app/layout.tsx
+++ plug-ui/app/layout.tsx
@@ -22,7 +22,16 @@ export default function RootLayout({
         <Providers>
           <div className="text-foreground bg-background gradient-radial text-base">
             <Header />
-            <main className="flex min-h-screen mt-3 w-dvw py-2 lg:px-24 sm:px-12 xs:px-6">
+            {process.env.NEXT_TEST_MODE === "true" && (<div className=" block w-3/4 m-2 mx-auto">
+                 
+                    <div className="w-4/5 mx-auto my-2 p-6 bg-danger-300 dark:bg-danger-200 text-3xl  rounded-lg shadow-lg">
+                        <strong>Uwaga!</strong>
+                        <p>To jest wersja testowa aplikacji w celu dołączenia do Stowarzyszenia, prosimy korzystać z dotychczasowego formularza zgłoszeniowego
+                          dostępnego <a className="underline" href="https://plug.org.pl/zostan-czlonkiem/" target="_blank">tutaj</a>.
+                        </p>                   
+                  </div>
+              </div>)}
+            <main className="flex min-h-screen mt-3 w-dvw py-2 lg:px-24 sm:px-12 xs:px-6">            
               {children}
             </main>
           </div>
