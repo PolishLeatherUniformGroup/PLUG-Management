b6d7f44e79f1f3950c95714a5638842e6bd4b4ef
Tomasz Molis <molitom@chrobinson.com>
2024-02-21 17:36:31 +0100

    reponsive

--- plug-ui/app/layout.tsx
+++ plug-ui/app/layout.tsx
@@ -24,16 +24,17 @@ export default function RootLayout({
           <SpeedInsights />
           <div className="text-foreground bg-background gradient-radial text-base">
             <Header />
-            {process.env.NEXT_TEST_MODE === "true" && (<div className=" block w-3/4 m-2 mx-auto">
+            {process.env.NEXT_TEST_MODE === "true" && (<div className="flex min-h-6 mt-1 md:mt-3 w-full py-2 px-2">
                  
-                    <div className="w-4/5 mx-auto my-2 p-6 bg-danger-300 dark:bg-danger-200 text-3xl  rounded-lg shadow-lg">
+                    <div className="w-full sm:w-4/5 xl:w-2/3 mx-auto my-2 p-3 bg-danger-300 dark:bg-danger-200 text-base md:text-lg xl:text-2xl rounded-lg shadow-lg">
                         <strong>Uwaga!</strong>
                         <p>To jest wersja testowa aplikacji w celu dołączenia do Stowarzyszenia, prosimy korzystać z dotychczasowego formularza zgłoszeniowego
                           dostępnego <a className="underline" href="https://plug.org.pl/zostan-czlonkiem/" target="_blank">tutaj</a>.
                         </p>                   
                   </div>
               </div>)}
-            <main className="flex min-h-screen mt-3 w-dvw py-2 lg:px-24 sm:px-12 xs:px-6">            
+            <main className="flex min-h-screen mt-3 w-full py-2 px-6 sm:px-3 xs:px-1">          
+            
               {children}
             </main>
           </div>
--- plug-ui/app/page.tsx
+++ plug-ui/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null
   }
-  return (<div className="w-3/4 mx-auto grid grid-cols-12 grid-rows-4 gap-2">
-    <Card className="col-start-2 col-span-6 h-[600px] row-span-4 shadow-xl" isBlurred isFooterBlurred={false}>
+  return (<div className="w-full lg:w-4/5 grid grid-cols-12 grid-rows-4 gap-1 md:gap-2 mx-auto">
+    <Card className="col-span-12 md:col-start-1 xl:col-start-2 md:col-span-10 md:col-span-12 xl:col-span-7 h-[480px] lg:h-[500px] xl:h-[640px] shadow-xl row-span-4" isBlurred isFooterBlurred={false}>
       <Image
         removeWrapper
         alt="Card background"
@@ -31,8 +31,8 @@ export default function Home() {
         <Button radius="full" size="sm" as="a" href="https://plug.org.pl/" target="_blank">Idź do www</Button>
       </CardFooter>
     </Card>
-    <div className="col-start-8 col-span-4 row-span-2 grid grid-cols-subgrid col-span-4 gap-2">
-      <Card className="col-span-4 h-[320px] shadow-xl row-span-2" isBlurred isFooterBlurred={false}>
+    <div className="col-span-12 xl:col-span-3 row-span-2 grid grid-cols-subgrid gap-2">
+      <Card className="col-span-12 md:col-span-8 lg:col-span-6 h-[360px] lg:h-[320px] shadow-xl  row-span-2 md:row-span-3" isBlurred isFooterBlurred={false}>
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-small text-white/60 uppercase font-bold">Dołącz do nas</p>
           <h4 className="text-white font-medium text-xl">Zostań członkiem Stowarzyszenia</h4>
@@ -51,43 +51,43 @@ export default function Home() {
         </CardFooter>
       </Card>
 
-        <Card className="bg-gradient-to-tr from-amber-500 to-pink-500 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://www.instagram.com/plug.group.official/') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faInstagram} className="text-white fa-3x m-auto " />
-          </CardBody>
-        </Card>
-        <Card className="bg-gradient-to-tr from-cyan-500 to-blue-500 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://twitter.com/plugPL') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faXTwitter} className="text-white fa-3x m-auto" />
-          </CardBody>
-        </Card>
-        <Card className="bg-gradient-to-tr from-red-700 to-red-500 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://www.youtube.com/@polishleatheruniformgroup') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faYoutube} className="text-white fa-3x m-auto" />
-          </CardBody>
-        </Card>
-        <Card className="bg-gradient-to-tr from-sky-600 to-blue-800 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://www.facebook.com/groups/plug.group') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faFacebook} className="text-white fa-3x m-auto" />
-          </CardBody>
-        </Card>
-        <Card className="bg-gradient-to-br from-lime-400 to-lime-600 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://chat.whatsapp.com/G5tB5UlmyfUK9fptMDabf3') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faWhatsapp} className="text-white fa-3x m-auto" />
-          </CardBody>
-        </Card>
-        <Card className="bg-gradient-to-b  from-indigo-500 to-indigo-700 aspect-square m-auto w-full"
-          isPressable onPress={() => { openInNewTab('https://discord.gg/Pay6cXcj') }}>
-          <CardBody>
-            <FontAwesomeIcon icon={faDiscord} className="text-white fa-3x m-auto" />
-          </CardBody>
-        </Card>
-    
+      <Card className="bg-gradient-to-tr from-amber-500 to-pink-500 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://www.instagram.com/plug.group.official/') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faInstagram} className="text-white fa-3x m-auto " />
+        </CardBody>
+      </Card>
+      <Card className="bg-gradient-to-tr from-cyan-500 to-blue-500 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://twitter.com/plugPL') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faXTwitter} className="text-white fa-3x m-auto" />
+        </CardBody>
+      </Card>
+      <Card className="bg-gradient-to-tr from-red-700 to-red-500 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://www.youtube.com/@polishleatheruniformgroup') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faYoutube} className="text-white fa-3x m-auto" />
+        </CardBody>
+      </Card>
+      <Card className="bg-gradient-to-tr from-sky-600 to-blue-800 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://www.facebook.com/groups/plug.group') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faFacebook} className="text-white fa-3x m-auto" />
+        </CardBody>
+      </Card>
+      <Card className="bg-gradient-to-br from-lime-400 to-lime-600 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://chat.whatsapp.com/G5tB5UlmyfUK9fptMDabf3') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faWhatsapp} className="text-white fa-3x m-auto" />
+        </CardBody>
+      </Card>
+      <Card className="bg-gradient-to-b  from-indigo-500 to-indigo-700 aspect-square m-auto w-full col-span-2 xl:col-span-1"
+        isPressable onPress={() => { openInNewTab('https://discord.gg/Pay6cXcj') }}>
+        <CardBody>
+          <FontAwesomeIcon icon={faDiscord} className="text-white fa-3x m-auto" />
+        </CardBody>
+      </Card>
+
     </div>
   </div>
   );
